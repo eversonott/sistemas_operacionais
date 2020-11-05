179a850455a2506712271ceecec4ec288dbb992b
Leticia Penha <augustoleticia@hotmail.com>
2020-11-04 22:40:50 -0300

    adição de comentarios no codigo

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.by import By
 from TomatoesDTO import TomatoesDTO
 import csv
 
-
+# Main
 genre = int(input("""Menu:
                1 - Ação
                2 - Animação
@@ -28,21 +28,32 @@ genre = int(input("""Menu:
 
 def genre_number_switcher(x):
     switcher = {
-         1: "1",
-         2: "2",
-         3: "4",
-         4: "5",
-         5: "6",
-         6: "8",
-         7: "9",
-         8: "10",
-         9: "11",
-         10: "13",
-         11: "18",
-         12: "14",
-         13: "1;2;4;5;6;8;9;10;11;13;18;14"
+        1: "1",
+        2: "2",
+        3: "4",
+        4: "5",
+        5: "6",
+        6: "8",
+        7: "9",
+        8: "10",
+        9: "11",
+        10: "13",
+        11: "18",
+        12: "14",
+        13: "1;2;4;5;6;8;9;10;11;13;18;14"
     }
     return switcher.get(x, "1;2;4;5;6;8;9;10;11;13;18;14")
+
+
+def write_csv(rotten_tomatoes_list):
+    with open('lista_filmes.csv', 'w', newline='') as csv_file:
+        fieldnames = ['Nome filme', 'audiência']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+        writer.writeheader()
+
+        for movie in rotten_tomatoes_list:
+            writer.writerow({movie.name, movie.audiencescore})
 
 
 url = 'https://www.rottentomatoes.com/browse/dvd-streaming-all?minTomato=0&maxTomato=100' \
@@ -51,32 +62,21 @@ url = 'https://www.rottentomatoes.com/browse/dvd-streaming-all?minTomato=0&maxTo
 driver = webdriver.Firefox()
 driver.get(url)
 time.sleep(10)
+
+# Busca na pagina principal todos os links dos filmes
 listMovie = driver.find_elements_by_tag_name("a")
-distinctListMovie = list(set(listMovie))
+links = [link.get_attribute('href') for link in listMovie]
 
-
-
-def write_csv(rotten_tomatoes_list):
-    with open('lista_filmes_rottenTomatoes.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-
-        writer.writerow(["Nome filme", "Nota audiência"])
-
-        for movie in rotten_tomatoes_list:
-            writer.writerow({movie.name, movie.audiencescore})
-
-links = [link.get_attribute('href') for link in distinctListMovie]
-movieList = list()
-
+# Cria uma lista com urls distintas
+movieList = set()
 for movieLink in links:
-    if not movieLink is None:
+    if movieLink is not None:
         if "/m/" in movieLink:
-            print(movieLink)
-           # movieList.append(movieLink)
+            movieList.add(movieLink)
 
-
+# Cria uma lista de objetos e faz o scrapping dos atributos da pagina do filme
 tomatoesList = []
-#def scrapping_movie_page(movieUrl):
+# def scrapping_movie_page(movieUrl):
 for movieUrl in movieList:
     driver.get(movieUrl)
     time.sleep(2)
@@ -84,24 +84,13 @@ for movieUrl in movieList:
         name = driver.find_element_by_css_selector("h1[data-qa='score-panel-movie-title']").text
         percentage = driver.find_elements_by_class_name("mop-ratings-wrap__percentage").__getitem__(1).text
     except IndexError:
-        percentage = "Em breve nas plataformas de streaming"
-    print(name)
+        percentage = "Ainda não possui nota da audiência"
     tomatoesList.append(TomatoesDTO(name, percentage))
-    print(percentage)
 
-
+#tomatoes_list_sort_by_score = sorted(tomatoesList, key=lambda tomatoes: tomatoesList.audiencescore)
 write_csv(tomatoesList)
 
-for item in tomatoesList:
-    print(item)
-
-#with ThreadPoolExecutor() as executor:
+# with ThreadPoolExecutor() as executor:
 #    futures = []
 #    for url in movieList:
 #       executor.submit(scrapping_movie_page, url)
-
-
-
-
-
-
